05c2afa27bfe40591b7000877bec6966e13425c0
Minh-Long  Do <minhlong.langos@gmail.com>
2019-03-23 21:39:39 +0100

    Refactor code

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,26 +1,29 @@
 require 'rspec'
 
 describe 'navigate', type: :feature do
+  before do
+    visit posts_path
+  end
+
   describe 'index' do
     it 'can be reached successfully' do
-      visit posts_path
       expect(page.status_code).to equal(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
     end
   end
 
   describe 'creation' do
-    it 'has a new form that can be reached' do
+    before do
       visit new_post_path
+    end
+    it 'has a new form that can be reached' do
       expect(page.status_code).to equal(200)
     end
 
     it 'can be created from new form page' do
-      visit new_post_path
 
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
