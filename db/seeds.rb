534d0503bbedf52e23ad8946973ed08a1986c279
Minh-Long  Do <minhlong.langos@gmail.com>
2019-03-24 21:27:47 +0000

    Create post table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
+@user = User.create(email: "test@test.com",
+                    password: "1234567",
+                    password_confirmation: "1234567",
+                    first_name: "Jon",
+                    last_name: "Snow")
+
+puts "1 User created"
+
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content")
+  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,6 +2,13 @@ require 'rspec'
 
 describe 'navigate', type: :feature do
   before do
+    @user = User.create(email: "test@test.com",
+                       password: "1234567",
+                       password_confirmation: "1234567",
+                       first_name: "Jon",
+                       last_name: "Snow")
+    @user.save
+    login_as(@user, :scope => :user)
     visit posts_path
   end
 
@@ -13,18 +20,24 @@ describe 'navigate', type: :feature do
     it 'has a title of Posts' do
       expect(page).to have_content(/Posts/)
     end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1")
+      post1.user = @user
+      post1.save
+      post2 = Post.create(date: Date.today, rationale: "Post2")
+      post2.user = @user
+      post2.save
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
+    end
   end
 
   describe 'creation' do
     before do
-      user = User.create(email: "test@test.com",
-                         password: "1234567",
-                         password_confirmation: "1234567",
-                         first_name: "Jon",
-                         last_name: "Snow")
-      login_as(user, :scope => :user)
       visit new_post_path
     end
+
     it 'has a new form that can be reached' do
       expect(page.status_code).to equal(200)
     end
